8d24633c530228ac4d3ea30fd9e263ad601763cc
Philipp Aleksovski <phal@ivu.de>
2026-09-20 16:28:23 +0200

    Fix CoreAudio input channel layout property

--- audio/out/ao_coreaudio.c
+++ audio/out/ao_coreaudio.c
@@ -324,11 +324,11 @@ static bool init_audiounit(struct ao *ao, AudioStreamBasicDescription asbd, Audi
                      "can't link audio unit to selected device");
 
     err = AudioUnitSetProperty(p->audio_unit,
-                               kAudioOutputUnitProperty_ChannelMap,
-                               kAudioUnitScope_Global, 0, layout, layout_size);
+                               kAudioUnitProperty_AudioChannelLayout,
+                               kAudioUnitScope_Input, 0, layout, layout_size);
 
     CHECK_CA_ERROR_L(coreaudio_error_audiounit,
-                     "unable to set the input channel layout on the audio unit");
+                     "unable to set input audio channel layout on audio unit");
 
     AURenderCallbackStruct render_cb = (AURenderCallbackStruct) {
         .inputProc       = render_cb_lpcm,
